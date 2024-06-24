f885086630ca28ccbffa30f41151a91c4965981c
daniel khait <dany0234@gmail.com>
2024-06-24 21:55:11 +0300

    Add webconfig file for Azure deployment

--- obj/Debug/net8.0/PdfInvoiceProcessor.AssemblyInfo.cs
+++ obj/Debug/net8.0/PdfInvoiceProcessor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PdfInvoiceProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80bbda21bc312872375852e0077a74bf64bc201")]
 [assembly: System.Reflection.AssemblyProductAttribute("PdfInvoiceProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PdfInvoiceProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
